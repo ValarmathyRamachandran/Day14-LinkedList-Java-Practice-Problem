a2e6649894a9135a60cebd71331c8fe4c64b7cec
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-10-21 18:52:07 +0530

    created Simple Linked List of 56,30,70

--- LinkedListUC1.java
+++ LinkedListUC1.java
@@ -1,14 +1,18 @@
 package bridgeLabz.Java.Practice;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class LinkedListUC1 { 
   public static void main(String[] args) { 
     LinkedList<Integer> list = new LinkedList<Integer>();
-    list.add(56);
-    list.add(30);
     list.add(70);
+    list.add(30);
+    list.add(56);
+    
     System.out.println(list);
+    
   } 
 } 	
+
 
 	
